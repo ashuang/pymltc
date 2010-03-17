1d56eafed3ed5587b611d483af88da784dea2860
root <root@92cfaad1-8e11-0410-ad78-0909e60bc13c>
2010-03-17 03:32:31 +0000

    (split) add facility codes
    git-svn-id: file:///office/svnroot@211 92cfaad1-8e11-0410-ad78-0909e60bc13c

--- medltc/ansi837p.py
+++ medltc/ansi837p.py
@@ -388,6 +388,41 @@ def isFileAnsi837p(file_obj):
         return False
     return True
 
+__facility_codes = { \
+        '11' : 'Office',
+        '12' : 'Home',
+        '21' : 'Inpatient Hospital',
+        '22' : 'Outpatient Hospital',
+        '23' : 'Emergency Room - Hospital',
+        '24' : 'Ambulatory Surgical Center',
+        '25' : 'Birthing Center',
+        '26' : 'Military Treatment Facility',
+        '31' : 'Skilled Nursing Facility',
+        '32' : 'Nursing Facility',
+        '33' : 'Custodial Care Facility',
+        '34' : 'Hospice',
+        '41' : 'Ambulance - Land',
+        '42' : 'Ambulance - Air or Water',
+        '51' : 'Inpatient Psychiatric Facility',
+        '52' : 'Psychiatric Facility Partial Hospitalization',
+        '53' : 'Community Mental Health Center',
+        '54' : 'Intermediate Care Facility/Mentally Retarded',
+        '55' : 'Residential Substance Abuse Treatment Facility',
+        '56' : 'Psychiatric Residential Treatment Center',
+        '50' : 'Federally Qualified Health Center',
+        '60' : 'Mass Immunization Center',
+        '61' : 'Comprehensive Inpatient Rehabilitation Facility',
+        '62' : 'Comprehensive Outpatient Rehabilitation Facility',
+        '65' : 'End Stage Renal Disease Treatment Facility',
+        '71' : 'State or Local Public Health Clinic',
+        '72' : 'Rural Health Clinic',
+        '81' : 'Independent Laboratory',
+        '99' : 'Other Unlisted Facility',
+}
+
+def facilityCodeToStr(code):
+    return __facility_codes[code]
+
 def usage():
     usage_str="""usage:  ansi837p.py [options] <filename>
 
